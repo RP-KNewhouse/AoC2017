a6779772f6af1d81010a265a67ba39962c9c1a4a
Kevin Newhouse <knewhouse@rightpoint.com>
2017-12-06 00:00:23 -0600

    6a

---
+++ 6/6a.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const inputString = fs.readFileSync('6a.txt', 'utf8');
+
+let initBank = inputString.split("\t");
+let bank = [];
+for (let i in initBank) {
+    bank.push(parseInt(initBank[i]));
+}
+
+let previousBanks = [];
+while (previousBanks.indexOf(JSON.stringify(bank)) === -1) {
+    previousBanks.push(JSON.stringify(bank));
+    let max = Math.max.apply(Math, bank);
+    let index = bank.indexOf(max);
+    bank[index] = 0;
+    for (let i = 1; i <= max; i++) {
+        bank[(index+i)%(bank.length)]++;
+    }
+}
+console.log(previousBanks.length);
